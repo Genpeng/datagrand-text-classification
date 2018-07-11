cf66c37fceee948cf07a124348a92590354d2ea1
Genpeng <xgp1227@gmail.com>
2018-07-11 21:44:02 +0800

    Change the code to fit datagrand data

--- models/01_textcnn_nonstatic/train.py
+++ models/01_textcnn_nonstatic/train.py
@@ -16,18 +16,18 @@ from text_cnn_nonstatic import TextCNN
 
 # Data loading params
 tf.flags.DEFINE_float("dev_sample_percentage", 0.1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("positive_data_file", "../../raw_data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data")
-tf.flags.DEFINE_string("negative_data_file", "../../raw_data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data")
+tf.flags.DEFINE_string("data_file", "../../raw_data/demo.csv", "Data source")
 
 # Model hyperparameter
-tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
-tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: 3,4,5)")
-tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 256, "Dimensionality of character embedding (default: 256)")
+tf.flags.DEFINE_string("filter_sizes", "2,3,4", "Comma-separated filter sizes (default: 2,3,4)")
+tf.flags.DEFINE_integer("num_filters", 256, "Number of filters per filter size (default: 256)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
 tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 0.0)")
 
 # Training parameters
-tf.flags.DEFINE_integer("batch_size", 64, "Batch size (default: 64)")
+tf.flags.DEFINE_integer("sequence_length", 3072, "The length of sentence (default: 3072)")
+tf.flags.DEFINE_integer("batch_size", 128, "Batch size (default: 128)")
 tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
@@ -46,10 +46,10 @@ def preprocess():
 
     # Load data
     print("Loading data...")
-    x_text, y = data_helper.load_data_and_labels(FLAGS.positive_data_file, FLAGS.negative_data_file)
+    x_text, y = data_helper.load_data_and_labels(FLAGS.data_file)
 
     # Build vocabulary
-    max_document_length = max([len(x.split()) for x in x_text])
+    max_document_length = FLAGS.sequence_length
     vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
     x = np.array(list(vocab_processor.fit_transform(x_text)))
 
