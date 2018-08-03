74e0f37b230555d58ee1d72542840203b783e1e6
Genpeng <xgp1227@gmail.com>
2018-08-03 15:32:43 +0800

    Modify pre-process procedure

---
+++ src/preprocess/resave_embeddings.py
@@ -0,0 +1,81 @@
+# _*_ coding: utf-8 _*_
+
+"""
+Re-save the file of word (character) embeddings to `npy` format and
+construct the mapping between words and its corresponding indices.
+
+Author: StrongXGP (xgp1227@gmail.com)
+Date:   2018/07/29
+"""
+
+import pickle
+import numpy as np
+
+EMBEDDING_SIZE = 300
+SPECIAL_SYMBOLS = ['<PAD>', '<UNK>']
+
+
+def load_embedding(embedding_file):
+    """Load embeddings from file."""
+    np.random.seed(42)
+
+    with open(embedding_file, 'r', encoding='utf-8') as f:
+        lines = f.read().splitlines()[1:]  # remove first line (embedding description line)
+
+    id_to_symbol_map = dict()
+    symbol_to_id_map = dict()
+    for i, symbol in enumerate(SPECIAL_SYMBOLS):
+        id_to_symbol_map[i] = symbol
+        symbol_to_id_map[symbol] = i
+
+    num_total_symbols = len(SPECIAL_SYMBOLS) + len(lines)
+    embeddings = np.zeros((num_total_symbols, EMBEDDING_SIZE), dtype=np.float32)
+    embeddings[1] = np.random.randn(EMBEDDING_SIZE)  # the values of 'UNK' satisfy the normal distribution
+
+    index = 2
+    for line in lines:
+        cols = line.split()
+        id_to_symbol_map[index] = cols[0]
+        symbol_to_id_map[cols[0]] = index
+        embeddings[index] = np.array(cols[1:], dtype=np.float32)
+        index += 1
+
+    return id_to_symbol_map, symbol_to_id_map, embeddings
+
+
+def main():
+    print("[INFO] Load character embeddings")
+    char_embedding_file = "../../embeddings/datagrand-char-300d.txt"
+    id_to_char_map, char_to_id_map, char_embeddings = load_embedding(char_embedding_file)
+    print("[INFO] Finished!")
+
+    print("[INFO] Save character embeddings")
+    id_to_char_file = "../../embeddings/id2char.pkl"
+    char_to_id_file = "../../embeddings/char2id.pkl"
+    char_embedding_resave_file = "../../embeddings/char-embedding-300d.npy"
+    with open(id_to_char_file, 'wb') as fout:
+        pickle.dump(id_to_char_map, fout)
+    with open(char_to_id_file, 'wb') as fout:
+        pickle.dump(char_to_id_map, fout)
+    np.save(char_embedding_resave_file, char_embeddings)
+    print("[INFO] Finish!")
+
+    word_embedding_file = "../../embeddings/datagrand-word-300d-mc5.txt"
+    id_to_word_map, word_to_id_map, word_embeddings = load_embedding(word_embedding_file)
+
+    word_embedding_file = "../../embeddings/datagrand-word-300d-mc5.txt"
+
+
+# Save to file
+# ===========================================================================================
+
+print("Save to file...")
+id2word_file = "../../processed_data/id2word.pkl"
+word2id_file = "../../processed_data/word2id.pkl"
+word_embeddings_file = "../../word_vectors/word-embedding-300d-mc5.npy"
+with open(id2word_file, 'wb') as fout:
+    pickle.dump(id_to_word_map, fout)
+with open(word2id_file, 'wb') as fout:
+    pickle.dump(word_to_id_map, fout)
+np.save(word_embeddings_file, word_embeddings)
+print("Finished! ( ^ _ ^ ) V")
--- src/preprocess/train_char_embeddings.py
+++ src/preprocess/train_char_embeddings.py
@@ -1,7 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 """
-Train distributed representation of words by using gensim.
+Train distributed representation of characters by using gensim.
 
 Author: StrongXGP
 Date:	2018/07/13
@@ -12,18 +12,21 @@ from time import time
 from gensim.models.word2vec import Word2Vec
 
 
-def load_word_samples(train_data_file, test_data_file):
-    """Load training and testing data, get the words of each sample in two dataset and return."""
+def load_char_samples(train_data_file, test_data_file):
+    """Load training and testing data, get the characters of each sample in two dataset and return."""
     train_lines = open(train_data_file, 'r', encoding='utf-8').read().splitlines()[1:]
     test_lines = open(test_data_file, 'r', encoding='utf-8').read().splitlines()[1:]
 
-    train_word_samples = [line.split(',')[2] for line in train_lines]
-    test_word_samples = [line.split(',')[2] for line in test_lines]
-    word_samples = train_word_samples + test_word_samples
+    train_char_samples = [line.split(',')[1] for line in train_lines]
+    test_char_samples = [line.split(',')[1] for line in test_lines]
+    char_samples = train_char_samples + test_char_samples
 
-    word_samples = [word_sample.split() for word_sample in word_samples]
+    char_samples = [char_sample.split() for char_sample in char_samples]
 
-    return word_samples
+    del train_lines, test_lines, train_char_samples, test_char_samples
+    gc.collect()
+
+    return char_samples
 
 
 def batch_iter(data, batch_size=5000):
@@ -43,25 +46,25 @@ def main():
     print("[INFO] Loading data...")
     train_data_file = "../../raw_data/train_set.csv"
     test_data_file = "../../raw_data/test_set.csv"
-    sentences = load_word_samples(train_data_file, test_data_file)
+    sentences = load_char_samples(train_data_file, test_data_file)
     print("[INFO] The total number of samples is: %d" % len(sentences))
 
     # Calculate the size of vocabulary
     # =========================================================================
 
-    words = []
+    chars = []
     for sentence in sentences:
-        words.extend(sentence)
-    print("[INFO] The total number of words is: %d" % len(set(words)))
+        chars.extend(sentence)
+    print("[INFO] The total number of characters is: %d" % len(set(chars)))
 
-    del words
+    del chars
     gc.collect()
 
     # Train and save word2vec model
     # =========================================================================
 
-    # Initialize word2vec model
-    model = Word2Vec(size=300, min_count=5, sg=0, iter=1, workers=16, seed=42)
+    print("[INFO] Initialize word2vec model...")
+    model = Word2Vec(size=300, min_count=1, sg=0, iter=30, workers=16, seed=42)
     model.build_vocab(sentences)
     print("[INFO] ", end='')
     print(model)
@@ -75,8 +78,8 @@ def main():
     print("[INFO] Training finished! ( ^ _ ^ ) V")
 
     print("[INFO] Save to file...")
-    model.wv.save("../../embeddings/datagrand-word-300d-mc5.bin")
-    model.wv.save_word2vec_format("../../embeddings/datagrand-word-300d-mc5.txt", binary=False)
+    model.wv.save("../../embeddings/datagrand-char-300d.bin")
+    model.wv.save_word2vec_format("../../embeddings/datagrand-char-300d.txt", binary=False)
     print("[INFO] Finished!")
 
 
