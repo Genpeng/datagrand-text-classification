5cc43ec90d1efe5c15c848b3324afa4400408d8a
Genpeng <xgp1227@gmail.com>
2018-08-07 09:11:48 +0800

    0.3.7

--- src/preprocess/transform_data_text.py
+++ src/preprocess/transform_data_text.py
@@ -7,9 +7,69 @@ Author: StrongXGP (xgp1227@gmail.com)
 Date:   2018/07/30
 """
 
+import pickle
+import numpy as np
 import pandas as pd
 from time import time
-from ..utils.data_utils import *
+
+# Global variable
+PAD_STR = '<PAD>'
+
+
+def load_mapping_table(mapping_table_file):
+    """Load the mapping table from words (characters) to its corresponding ids."""
+    with open(mapping_table_file, 'rb') as fin:
+        mapping_table = pickle.load(fin)
+    return mapping_table
+
+
+def load_samples_and_labels(data_path, header=True, col=1, train=True):
+    """Load words (or characters) and its label of all the samples."""
+    if header:
+        start_index = 1
+    else:
+        start_index = 0
+
+    with open(data_path, 'r', encoding='utf-8') as f:
+        lines = f.read().splitlines()[start_index:]
+        samples = [line.split(',')[col] for line in lines]
+        samples = [sample.split() for sample in samples]
+
+    if train:
+        labels = [int(line.split(',')[3]) for line in lines]
+    else:
+        labels = []
+
+    return samples, labels
+
+
+def truncate(data, sequence_length=3000):
+    """Truncate the words (characters) of each sample to a fixed length."""
+    res = []
+    for sample in data:
+        if len(sample) > sequence_length:
+            sample = sample[:sequence_length]
+            res.append(sample)
+        else:
+            str_added = [PAD_STR] * (sequence_length - len(sample))
+            sample += str_added
+            res.append(sample)
+    return res
+
+
+def transform_to_ids(data, word_to_id_map):
+    """Transform the words (characters) of a sample to its ids."""
+    res = list()
+    for words in data:
+        ids = list()
+        for word in words:
+            ids.append(word_to_id_map.get(word, 1))  # 1 is the id of '<UNK>'
+            # if word in word_to_id_map:
+            #     ids.append(word_to_id_map[word])
+            # else:
+            #     ids.append(1)  # 1 is the id of '<UNK>'
+        res.append(ids)
+    return res
 
 
 def transform_characters():
@@ -41,7 +101,7 @@ def transform_characters():
     t0_transform = time()
     ids_train = transform_to_ids(chars_train, char_to_id_map)
     ids_test = transform_to_ids(chars_test, char_to_id_map)
-    print("Done in %.3f seconds." % (time() - t0_transform))
+    print("[INFO] Done in %.3f seconds." % (time() - t0_transform))
     print("[INFO] Finished!")
 
     # Save to file
@@ -83,8 +143,10 @@ def transform_words():
     print("[INFO] Finished!")
 
     print("[INFO] Transform to ids...")
+    t0_transform = time()
     ids_train = transform_to_ids(words_train, word_to_id_map)
     ids_test = transform_to_ids(words_test, word_to_id_map)
+    print("[INFO] Done in %.3f seconds." % (time() - t0_transform))
     print("[INFO] Finished!")
 
     # Save to file
